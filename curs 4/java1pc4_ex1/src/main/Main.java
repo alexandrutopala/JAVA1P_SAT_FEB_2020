ba650d97996844b21e7113bc9ee6304f3b513cf2
alexandru.topala <alexandru.top98@gmail.com>
2020-04-10 08:39:42 +0300

    Rezolvare exercitiu curs 4

--- curs 4/java1pc4_ex1/src/main/Main.java
+++ curs 4/java1pc4_ex1/src/main/Main.java
@@ -1,5 +1,17 @@
 package main;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /*
     Se dau:
         - bundle-ul "hello" cu fisiere de proprietati
@@ -18,7 +30,50 @@ package main;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Path currentDirectory = Paths.get(".");
 
+        Files.list(currentDirectory)
+                .filter(path -> path.getFileName().toString().startsWith("input"))
+                .forEach(path -> translateFile(path));
+    }
+
+    private static void translateFile(Path path) {
+        Locale locale = extractLocaleFromFileName(path.getFileName().toString());
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("hello", locale);
+
+        try {
+            String translatedLine =
+                Files.lines(path)
+                        .map(line -> line.split("\\s+"))
+                        .map(wordKeysArray -> List.of(wordKeysArray))
+                        .flatMap(wordKeysList -> wordKeysList.stream())
+                        .map(wordKey -> resourceBundle.getString(wordKey))
+                        .collect(Collectors.joining(" "));
+
+            Path destination = Paths.get("./trad_" + locale.toString() + ".txt");
+
+            Files.writeString(destination, translatedLine);
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Locale extractLocaleFromFileName(String fileName) {
+        String localeReggex = "[a-z][a-z]_[A-Z][A-Z]";
+        Pattern pattern = Pattern.compile(localeReggex);
+        Matcher matcher = pattern.matcher(fileName);
+
+        if (matcher.find()) {
+            int localeStartIndex = matcher.start();
+            int localeEndIndex = matcher.end();
+
+            String localeString = fileName.substring(localeStartIndex, localeEndIndex);
+
+            return new Locale(localeString);
+        }
+
+        throw new IllegalArgumentException("No locale found in file name");
     }
 }
